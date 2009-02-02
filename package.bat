eb17f7c8c2fbac529fe8d21ff818b0a4f04dedcf
aaubry <aaubry@25845490-acef-11dd-9a14-c1645b63b0c6>
2009-02-02 17:29:09 +0000

    Added: Signing code to the packaging script.

--- package.bat
+++ package.bat
@@ -12,10 +12,31 @@ REM Binary package
 MD bin
 
 FOR %%I IN (SixPack.Web.Services SixPack.Net.Mail SixPack) DO (
-	XCOPY /E ..\src\%%I\bin bin
+	XCOPY /E /Q ..\src\%%I\bin bin
 )
 
 COPY ..\src\LICENSE bin
+
+SET KEYFILE=..\SixPack.pfx
+SET ILMERGE=..\support\windows\ILMerge\ILMerge.exe
+
+IF EXIST "%KEYFILE%" (
+	IF EXIST "%ILMERGE%" (
+		FOR %%J IN (Debug Release) DO (
+			FOR %%I IN (bin/%%J/*.dll) DO (
+				ECHO Signing %%J\%%I
+				REN bin\%%J\%%I _%%I
+				"%ILMERGE%" bin\%%J\_%%I /keyfile:"%KEYFILE%" /out:bin\%%J\%%I
+				DEL bin\%%J\_%%I
+			)
+		)
+	) ELSE (
+		ECHO To enable signing of the assemblies, install ILMERGE into "%ILMERGE%".
+		ECHO Download ILMERGE from http://www.microsoft.com/downloads/details.aspx?familyid=22914587-b4ad-4eae-87cf-b14ae6a939b0&displaylang=en
+	)
+) ELSE (
+	ECHO To enable signing of the assemblies, place a PFX file named "%KEYFILE%" in the root folder.
+)
 
 ..\support\windows\7zip\7z.exe a ..\SixPack-bin-1.0.zip bin
 
@@ -26,8 +47,9 @@ ECHO. > exclusions
 ECHO \.svn\ >> exclusions
 ECHO \bin\ >> exclusions
 ECHO \obj\ >> exclusions
+ECHO \_Resharper >> exclusions
 
-XCOPY /I /E /EXCLUDE:exclusions ..\src src
+XCOPY /I /E /Q /EXCLUDE:exclusions ..\src src
 
 ..\support\windows\7zip\7z.exe a ..\SixPack-src-1.0.zip src
 
@@ -38,7 +60,7 @@ ECHO. > exclusions
 ECHO \.svn\ >> exclusions
 ECHO .chm >> exclusions
 
-XCOPY /I /E /EXCLUDE:exclusions ..\HelpTmp doc
+XCOPY /I /E /Q /EXCLUDE:exclusions ..\HelpTmp doc
 
 ..\support\windows\7zip\7z.exe a ..\SixPack-doc-html-1.0.zip doc
 
