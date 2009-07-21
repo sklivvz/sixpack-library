b2ce5e17f3bce01ff98fcc6d52557c0f1bd083ed
aaubry <aaubry@25845490-acef-11dd-9a14-c1645b63b0c6>
2009-07-21 14:44:15 +0000

    Modified: RegularExpressionPatterns values are now constants to allow them to be used as custom attribute parameters.

--- src/SixPack/Text/RegularExpressionPatterns.cs
+++ src/SixPack/Text/RegularExpressionPatterns.cs
@@ -11,16 +11,16 @@ namespace SixPack.Text
 		/// <summary>
 		/// Matches an hexadecimal string, composed of pairs of hexadecimal digits.
 		/// </summary>
-		public static readonly string HexString = @"^([0-9a-fA-F]{2})*$";
+		public const string HexString = @"^([0-9a-fA-F]{2})*$";
 
 		/// <summary>
 		/// Matches a base-64 string.
 		/// </summary>
-		public static readonly string Base64String = @"^([A-Za-z0-9+/]{4})*(([A-Za-z0-9+/]{3}=)|([A-Za-z0-9+/]{2}==)|([A-Za-z0-9+/]{1}===))?$";
+		public const string Base64String = @"^([A-Za-z0-9+/]{4})*(([A-Za-z0-9+/]{3}=)|([A-Za-z0-9+/]{2}==)|([A-Za-z0-9+/]{1}===))?$";
 
 		/// <summary>
 		/// Matches a base-64 string, as encoded by <see cref="HttpServerUtility.UrlTokenEncode"/>.
 		/// </summary>
-		public static readonly string UrlTokenString = @"^[A-Za-z0-9-_]*$";
+		public const string UrlTokenString = @"^[A-Za-z0-9-_]*$";
 	}
 }
